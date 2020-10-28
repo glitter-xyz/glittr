b49286468c3f11f2201c70ef10bf5525b220d185
Kiril Vatev <vatev.1@gmail.com>
2020-10-28 17:48:37 -0400

    adding a debug view

--- renderer/index.js
+++ renderer/index.js
@@ -1,6 +1,14 @@
 const confetti = require('canvas-confetti');
 const { ipcRenderer } = require('electron');
 
+const { css } = require('./tools/ui.js');
+
+css('./base.css');
+
+if (process.env.DEBUG_DAZZLE) {
+  document.querySelector('html').setAttribute('debug', true);
+}
+
 ipcRenderer.on('asynchronous-message', (ev, { command, x, y }) => {
   if (command !== 'draw') {
     return;

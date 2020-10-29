793ae0c68c6d5958f78b742b6e01c0dedc57573b
Kiril Vatev <vatev.1@gmail.com>
2020-10-29 18:15:53 -0400

    cleanup up macos code related to GUI apps
    it is not relevant here, because the app will quit when it is quit (I know, what a crazy thought)

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 const path = require('path');
 const url = require('url');
-const EventEmitter = require('events');
-const events = new EventEmitter();
 const iohook = require('iohook');
 
 const { app, BrowserWindow, screen, systemPreferences } = require('electron');
@@ -120,22 +118,3 @@ app.once('before-quit', () => {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform === 'darwin' || stayAlive) {
-    events.removeAllListeners();
-  } else {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
